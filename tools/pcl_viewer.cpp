ed247ead3badac843ff03024fb2e040d92bba72f
Diego Ramírez <diego_rd93@hotmail.com>
2015-10-16 18:23:25 -0300

    Added depth registration

--- tools/pcl_viewer.cpp
+++ tools/pcl_viewer.cpp
@@ -8,7 +8,7 @@
 
 class simple_visualizer {
 public:
-  simple_visualizer() : viewer("3D Viewer") {}
+  simple_visualizer() : viewer("3D Viewer", false) {}
 
   void run() {
     using pcl::PointCloud;
@@ -16,16 +16,20 @@ public:
     using pcl::OpenNIGrabber;
     using namespace std::chrono_literals;
 
-    viewer.setBackgroundColor(1, 1, 1);
-    viewer.addCoordinateSystem(1.0f, "global");
+    viewer.setFullScreen(true);
+    viewer.setWindowBorders(true);
+    viewer.setBackgroundColor(0, 0, 0);
     viewer.initCameraParameters();
     viewer.setCameraPosition(0.0, -0.3, -0.2, 0.0, -0.3, 1.0, 0.0, -1.0, 0.0);
+    viewer.createInteractor();
+    // register_keyboard_manager();
 
     auto interface = std::make_unique<OpenNIGrabber>();
 
-    boost::function<void(const PointCloud<PointXYZRGBA>::ConstPtr &)> cb = [&](
-        const auto &cloud) { f_claude = cloud; };
+    boost::function<void(const PointCloud<PointXYZRGBA>::ConstPtr &)> cb;
+    cb = [&](const auto &cloud) { f_claude = cloud; };
     interface->registerCallback(cb);
+    interface->getDevice()->setDepthRegistration(true);
 
     interface->start();
     while (!viewer.wasStopped()) {
@@ -39,6 +43,7 @@ public:
 private:
   void update_viewer() {
     using pcl::visualization::PCL_VISUALIZER_POINT_SIZE;
+    using pcl::visualization::PCL_VISUALIZER_LINE_WIDTH;
 
     auto claude = f_claude;
     if (!claude)
@@ -49,6 +54,20 @@ private:
     viewer.setPointCloudRenderingProperties(PCL_VISUALIZER_POINT_SIZE, 2,
                                             "claude");
   }
+
+  //  void register_keyboard_manager() {
+  //    using pcl::visualization::KeyboardEvent;
+  //    boost::function<void(const KeyboardEvent &)> callback;
+  //    callback = [](const KeyboardEvent &ev) {
+  //      std::cout << "KeyEv" << std::endl;
+  //      if (ev.keyUp())
+  //        return;
+  //      std::cout << "HOLA" << std::endl;
+  //      if (ev.getKeyCode() == 0)
+  //        std::cout << ev.getKeySym() << std::endl;
+  //    };
+  //    viewer.registerKeyboardCallback(callback);
+  //  }
 
 private:
   pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr f_claude;
