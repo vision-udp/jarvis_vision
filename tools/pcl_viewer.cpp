b1e616c7c4fde93efc0da6ec350458d8d76f7445
Diego Ramírez <diego_rd93@hotmail.com>
2015-10-18 22:37:01 -0300

    Added cloud processing

--- tools/pcl_viewer.cpp
+++ tools/pcl_viewer.cpp
@@ -3,16 +3,57 @@
 #include <thread>
 #include <chrono>
 
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl/io/openni_grabber.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_cylinder.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
+template <typename Point>
+void statistical_outliner_removal(
+    const typename pcl::PointCloud<Point>::ConstPtr &cloud, const int k,
+    const double stddev_mult, pcl::PointCloud<Point> &output_cloud) {
+  pcl::StatisticalOutlierRemoval<Point> sor;
+  sor.setInputCloud(cloud);
+  sor.setMeanK(k);
+  sor.setStddevMulThresh(stddev_mult);
+  sor.setNegative(true);
+  sor.filter(output_cloud);
+}
+
+template <typename Point>
+void downsample(const typename pcl::PointCloud<Point>::ConstPtr &cloud,
+                pcl::PointCloud<Point> &output_cloud) {
+  pcl::VoxelGrid<Point> filter;
+  filter.setInputCloud(cloud);
+  filter.setLeafSize(0.007f, 0.007f, 0.007f);
+  filter.filter(output_cloud);
+}
+
+template <typename Point>
+void find_cylinders(const typename pcl::PointCloud<Point>::ConstPtr &cloud,
+                    pcl::PointCloud<Point> &output_cloud) {
+  using model_t = pcl::SampleConsensusModelCylinder<Point, pcl::PointXYZ>;
+  auto model = boost::make_shared<model_t>(cloud);
+  pcl::RandomSampleConsensus<Point> ransac(model);
+  ransac.setDistanceThreshold(0.01);
+  ransac.computeModel();
+
+  std::vector<int> inliers;
+  ransac.getInliers(inliers);
+  pcl::copyPointCloud(*cloud, inliers, output_cloud);
+}
+
 class simple_visualizer {
+  using PointXYZRGBA = pcl::PointXYZRGBA;
+  using PointXYZ = pcl::PointXYZ;
+
 public:
   simple_visualizer() : viewer("3D Viewer", false) {}
 
   void run() {
     using pcl::PointCloud;
-    using pcl::PointXYZRGBA;
     using pcl::OpenNIGrabber;
     using namespace std::chrono_literals;
 
@@ -26,7 +67,7 @@ public:
 
     auto interface = std::make_unique<OpenNIGrabber>();
 
-    boost::function<void(const PointCloud<PointXYZRGBA>::ConstPtr &)> cb;
+    boost::function<void(const PointCloud<PointXYZ>::ConstPtr &)> cb;
     cb = [&](const auto &cloud) { f_claude = cloud; };
     interface->registerCallback(cb);
     interface->getDevice()->setDepthRegistration(true);
@@ -43,16 +84,29 @@ public:
 private:
   void update_viewer() {
     using pcl::visualization::PCL_VISUALIZER_POINT_SIZE;
-    using pcl::visualization::PCL_VISUALIZER_LINE_WIDTH;
 
     auto claude = f_claude;
     if (!claude)
       return;
 
-    if (!viewer.updatePointCloud(claude, "claude"))
-      viewer.addPointCloud(claude, "claude");
-    viewer.setPointCloudRenderingProperties(PCL_VISUALIZER_POINT_SIZE, 2,
-                                            "claude");
+    using cloud_t = pcl::PointCloud<pcl::PointXYZ>;
+    auto filtered_cloud = boost::make_shared<cloud_t>();
+
+    // downsample(claude, *filtered_cloud);
+    // statistical_outliner_removal(claude, 40, 2.00, *filtered_cloud);
+    find_cylinders(claude, *filtered_cloud);
+
+    //    using handler_t =
+    //        pcl::visualization::PointCloudColorHandlerCustom<PointXYZRGBA>;
+    //    handler_t handler(filtered_cloud, 255, 0, 0);
+    //
+    //    if (!viewer.updatePointCloud(claude, "francisco"))
+    //      viewer.addPointCloud(claude, "francisco");
+
+    if (!viewer.updatePointCloud(filtered_cloud, "claude"))
+      viewer.addPointCloud(filtered_cloud, "claude");
+    //    viewer.setPointCloudRenderingProperties(PCL_VISUALIZER_POINT_SIZE, 1,
+    //                                            "claude");
   }
 
   //  void register_keyboard_manager() {
@@ -70,7 +124,7 @@ private:
   //  }
 
 private:
-  pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr f_claude;
+  pcl::PointCloud<pcl::PointXYZ>::ConstPtr f_claude;
   pcl::visualization::PCLVisualizer viewer;
 };
 
