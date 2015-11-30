c8d67b751f83855d82ddbf0b6977c39e0d5d60aa
Jorge Aguirre <jorge.aguirre.sza@gmail.com>
2015-11-29 22:26:05 -0300

    add lu-transform example

---
+++ examples/textures/lu_transform.cpp
@@ -0,0 +1,97 @@
+/// \file main.cpp
+/// \brief Example of usage of texture descriptor LU-transform.
+/// \author Jorge Aguirre
+/// \version 1.0
+/// \date 2015-11-28
+
+#include <jarvis/extract_image.hpp>
+#include <jarvis/lu_transform.hpp>
+#include <jarvis/steady_timer.hpp>
+
+#include <iostream>
+#include <thread>
+
+#include <pcl/io/pcd_io.h>
+
+#include <boost/filesystem.hpp>
+
+using namespace jarvis;
+using namespace std::chrono_literals;
+namespace fs = boost::filesystem;
+
+using pcl::PointXYZRGBA;
+using std::clog;
+using pcl::console::print_error;
+using cloud_t = pcl::PointCloud<PointXYZRGBA>;
+using cloud_ptr = boost::shared_ptr<cloud_t>;
+
+static std::vector<fs::path> images_paths(const fs::path &input_path) {
+  if (fs::is_regular_file(input_path))
+    return std::vector<fs::path>({input_path});
+
+  std::vector<fs::path> frames_paths;
+  for (const auto &entry : fs::directory_iterator(input_path)) {
+    const auto &path = entry.path();
+    if (fs::is_regular_file(entry.status()) &&
+        (path.extension() == ".png" || path.extension() == ".jpg"))
+      frames_paths.push_back(path);
+  }
+
+  return frames_paths;
+}
+
+#define window_size 8
+
+static void transform_image(const fs::path &input_path, size_t spacing,
+                            bool show) {
+  cv::Mat image = cv::imread(input_path.c_str());
+  cv::Mat image_gray(image.size(), CV_8UC1);
+  steady_timer timer;
+
+  clog << "Image path: " << input_path << '\n';
+  clog << "dimensions: " << image.size() << '\n';
+  cv::cvtColor(image, image_gray, CV_BGR2GRAY);
+  image_gray.convertTo(image_gray, CV_64FC1);
+
+  auto params = lu_transform<double, window_size>(
+      image_gray, static_cast<ptrdiff_t>(window_size) / 2, spacing);
+  timer.finish();
+  int rows = image_gray.rows;
+  int cols = image_gray.cols;
+  if (spacing) {
+    rows /= static_cast<int>(spacing);
+    cols /= static_cast<int>(spacing);
+  }
+
+  cv::Mat result(rows, cols, image_gray.type(), params.data());
+  cv::Scalar mean;
+  cv::Scalar std_dev;
+  cv::meanStdDev(result, mean, std_dev);
+
+  std::printf("mean: %f\nstd dev: %f\nratio: %f\n", mean.val[0], std_dev.val[0],
+              std_dev.val[0] / mean.val[0]);
+
+  if (show) {
+    auto result_size = image.size();
+    result_size.width /= 2;
+    result_size.height /= 2;
+    cv::resize(result, result, result_size);
+    result.convertTo(result, CV_8UC1);
+    cv::imshow("LU-transform", result);
+    cv::waitKey();
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    clog << "Usage: " << argv[0] << " <input>\n";
+    return 1;
+  }
+
+  auto input_paths = images_paths(argv[1]);
+
+  const size_t spacing = 8;
+
+  for (auto &path : input_paths)
+    transform_image(path, spacing, true);
+}
--- include/jarvis/lu_transform.hpp
+++ include/jarvis/lu_transform.hpp
@@ -12,6 +12,7 @@
 
 #include <numeric>
 #include <iterator>
+#include <cassert>
 
 #include <pcl/common/common_headers.h>
 #include <opencv2/core/eigen.hpp>
@@ -27,41 +28,52 @@ Matrix lu_triangular_upper(Matrix &window, const cv::Mat &roi) {
 
 namespace jarvis {
 
-template <typename T, size_t W = 4>
-inline std::vector<T> lu_transform(const cv::Mat &mat, ptrdiff_t l) {
+template <typename T, size_t W = 8>
+inline std::vector<T> lu_transform(const cv::Mat &source, const ptrdiff_t l,
+                                   const size_t spacing) {
   using matrix = Eigen::Matrix<T, W, W>;
-  matrix window;
-
   const auto window_size = W;
-  const auto height = static_cast<size_t>(mat.rows) / window_size;
-  const auto width = static_cast<size_t>(mat.cols) / window_size;
+  assert(l > 0 && static_cast<size_t>(l) < window_size);
 
-  std::vector<T> result;
-  result.reserve(width * height);
-
-  auto roi = [&mat](size_t i, size_t j) {
-    int x = static_cast<int>(j * window_size);
-    int y = static_cast<int>(i * window_size);
-    return mat(cv::Rect(x, y, window_size, window_size));
+  matrix window;
+  auto apply_transform = [&](const cv::Mat &roi) {
+    assert(static_cast<size_t>(roi.cols) == window_size &&
+           static_cast<size_t>(roi.rows) == window_size);
+    Eigen::Matrix<T, W, 1> diag_u =
+        lu_triangular_upper(window, roi).diagonal().cwiseAbs();
+    std::vector<T> coeff(window_size);
+    Eigen::Map<Eigen::Matrix<T, W, 1>>(coeff.data(), diag_u.size()) = diag_u;
+    std::sort(coeff.begin(), coeff.end(), std::greater<T>());
+    auto begin = std::next(coeff.begin(), l);
+    auto end = coeff.end();
+    return std::accumulate(begin, end, 0);
   };
 
-  std::vector<T> coeff(window_size);
-  for (size_t i = 0; i < height; ++i)
-    for (size_t j = 0; j < width; ++j) {
-      matrix U = lu_triangular_upper(window, roi(i, j));
+  cv::Mat expanded;
+  const int anchor = static_cast<int>(window_size) / 2;
+  cv::copyMakeBorder(source, expanded, anchor, anchor, anchor, anchor,
+                     cv::BORDER_DEFAULT);
 
-      for (size_t k = 0; k < window_size; ++k) {
-        auto index = static_cast<long>(k);
-        coeff[k] = std::abs(U(index, index));
-      }
+  auto rows = static_cast<size_t>(source.rows);
+  auto cols = static_cast<size_t>(source.cols);
 
-      std::sort(coeff.begin(), coeff.end(), std::greater<T>());
+  if (spacing) {
+    rows /= spacing;
+    cols /= spacing;
+  }
 
-      auto begin = std::next(coeff.begin(), l);
-      auto end = coeff.end();
-      result[i * width + j] = std::accumulate(begin, end, 0);
+  std::vector<T> result;
+  result.reserve(rows * cols);
+
+  auto max_height = expanded.rows - anchor;
+  auto max_width = expanded.cols - anchor;
+  for (int i = anchor; i < max_height; i += spacing)
+    for (int j = anchor; j < max_width; j += spacing) {
+      auto roi = cv::Rect(j - anchor, i - anchor, window_size, window_size);
+      result.push_back(apply_transform(expanded(roi)));
     }
 
+  assert(result.size == rows * cols);
   return result;
 }
 }
